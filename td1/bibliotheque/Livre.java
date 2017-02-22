896d0cc5808207ef5f415583ee4f32daea0304ca
Mansour NIANG <smansour.niang@gmail.com>
2017-02-21 23:43:52 -0800

    update before starting course

--- td1/bibliotheque/Livre.java
+++ td1/bibliotheque/Livre.java
@@ -1,4 +1,32 @@
 
 public class Livre{
+	int isbn; 
+	String titre; 
+	String auteur; 
 
+	public Livre(int isbn, String titre, String auteur){
+		this.isbn = isbn; 
+		this.titre = titre;
+		this.auteur = auteur; 
+	}
+
+	public void getIsbn(){
+		return isbn; 
+	}
+	public void getTitre(){
+		return titre; 
+	}
+	public void getAuteur(){
+		return auteur; 
+	}
+
+	public int setIsbn(int i){
+		this.isbn = i; 
+	}
+	public int setTitre(int t){
+		this.titre = t; 
+	}
+	public int setAuteur(int a){
+		this.auteur = a; 
+	}
 }
