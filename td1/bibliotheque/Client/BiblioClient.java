3adbd8bf1c9093951c7072aeba11499265f81dab
nianse15 <nianse15@b048-14.crio.dauphine.fr>
2017-02-22 11:50:13 +0100

    Premiere version : Passage par valeur

---
+++ td1/bibliotheque/Client/BiblioClient.java
@@ -0,0 +1,30 @@
+import java.rmi.*;
+import java.rmi.registry.*;
+import java.util.List;
+import java.net.*;
+
+public class BiblioClient{
+
+	public static void main(String[] args){		
+		try {
+		    String codeBase = "file:///users/nianse15/Bureau/coursWebServices/td1/bibliotheque/Client/";
+		    System.setProperty("java.rmi.server.codebase", codeBase);
+		    System.setProperty("java.security.policy", "security.policy"); 
+		    System.setSecurityManager(new RMISecurityManager());
+
+		    Bibliotheque biblio = (Bibliotheque) Naming.lookup("rmi://localhost:1099/BibliothequeService");
+		    biblio.add(1, "Nommade", "Cheikh Anta DIOP");
+		    biblio.add(2, "L'enfant noir", "Camara LAYE");
+		    biblio.add(3, "Une si longue lettre", "Mariame BA");
+		    biblio.add(4, "Le ventre de l'atlantique", "Fatou DIOME");
+		    biblio.add(5, "La crise de 2008", "Jacques ATTALI");
+		    biblio.add(6, "Toto tape pathe", "Ben Omar");
+		    
+		    List<Book> list = biblio.findTitre("Fatou DIOME");		    
+		    System.out.println("Fatou DIOME a écrit "+ list);		    
+		    
+		} catch (Exception e) {
+		    System.err.println("Server exception: " + e.getMessage());		 
+		}
+	}
+}
---
+++ td1/bibliotheque/Client/Book.java~
@@ -0,0 +1,33 @@
+import java.io.Serializable; 
+
+public class Book implements Serializable{
+	private int isbn; 
+	private String titre; 
+	private String auteur; 
+
+	public Book(int isbn, String titre, String auteur){
+		this.isbn = isbn; 
+		this.titre = titre;
+		this.auteur = auteur; 
+	}
+
+	public int getIsbn(){
+		return isbn; 
+	}
+	public String getTitre(){
+		return titre; 
+	}
+	public String getAuteur(){
+		return auteur; 
+	}
+
+	public void setIsbn(int i){
+		this.isbn = i; 
+	}
+	public void setTitre(String t){
+		this.titre = t; 
+	}
+	public void setAuteur(String a){
+		this.auteur = a; 
+	}
+}
